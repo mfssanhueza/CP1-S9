1dfdec955f3fd3fabc3b8a7818c73cbc910c74cd
mfssanhueza <mfssanhueza@gmail.com>
2018-08-07 17:17:41 -0400

    ejercicio 4

--- ejercicio4.rb
+++ ejercicio4.rb
@@ -1,1 +1,18 @@
-propiedades = {nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café'}
+class Dog
+  def initialize(hash)
+    @hash = hash
+  end
+
+  def bark
+    puts "#{@hash.values[0]} está ladrando."
+  end
+
+  def show_information
+    puts "#{@hash.values[0]} es un perro de #{@hash.keys[1]} #{@hash.values[1]} y #{@hash.keys[2]} #{@hash.values[2].downcase}."
+  end
+end
+
+propiedades = { nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café' }
+dog = Dog.new(propiedades)
+puts dog.bark
+puts dog.show_information
